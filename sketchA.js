12506a8401ec15bf1a69850d8cc8f57351a726f2
secc9 <78687901+secc9@users.noreply.github.com>
2026-01-23 13:18:40 +0000

    adding p5,js library

---
+++ sketchA.js
@@ -0,0 +1,48 @@
+
+// sketch A
+// a random walk
+
+
+let posX, posY;
+let amt;
+let size = 900;
+function setup(){
+createCanvas(displayWidth, displayHeight);
+frameRate(30);
+background(255);
+
+noLoop();
+}
+
+
+function draw(){
+
+// get new postion by adding a random quantity to the current one
+
+let nPosX = posX + random(-amt, amt);
+let nPosY = posY + random(-amt, amt);
+
+
+//draw a line with current properties and coordinates
+noFill();
+strokeWeight(random(0.1, random(2,20)));
+stroke(random(0, 100), random(100, 200), random(155, 255));
+line(posX, posY, nPosX, nPosY);
+
+//update current position
+posX = nPosX;
+posY = nPosY;
+
+}
+
+
+// click moise to start drawing
+function mousePressed(){
+posX = mouseX;
+posY= mouseY;
+amt = random(4, 32);
+
+// from now on use the loop - it will cause the program to call draw()
+
+loop();
+}
